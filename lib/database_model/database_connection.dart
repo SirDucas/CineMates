6d5be5d82086b671a590564a6b00be142add4760
Paolo Migliozzi <pa.migliozzi@studenti.unina.it>
2021-11-03 18:38:17 +0100

    Cambio accesso db AWS

--- lib/database_model/database_connection.dart
+++ lib/database_model/database_connection.dart
@@ -12,10 +12,10 @@ class DatabaseConnection {
 
   Future<void> initConnection() async {
     this.conn = await MySqlConnection.connect(ConnectionSettings(
-        host: 'cinemates-db.cvtkh0bf6amk.us-east-2.rds.amazonaws.com',
+        host: 'cinemates-db.coh7z9zsf7dm.eu-west-1.rds.amazonaws.com',
         port: 3306,
-        user: 'root',
+        user: 'admin',
         db: 'cinemates_db',
-        password: 'cinemates123'));
+        password: 'cinemates12345'));
   }
 }
